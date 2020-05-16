8b9cdb7a836ffc4ca32ef8a4a5d5ea111e3537c8
Omowonuola <oyedmola@gmail.com>
2020-05-16 01:25:09 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function Todo({ todo, index, completeTodo, deleteTodo}) {
+  return <div style={{textDecoration: todo.isCompleted ? 'line-through' : ''}} className="todo">{todo.text}
+<div>
+  <button onClick={() => completeTodo(index)}>Complete</button>
+  <button onClick={() => deleteTodo(index)}>X</button>
+</div>
+</div>
+}
+
+function TodoForm({addTodo}) {
+  const [value, setValue] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value); 
+    setValue('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+        type='text' 
+        className='input' 
+        value={value} 
+        placeholder='Add a new Todo'
+        onChange={e => setValue(e.target.value)} 
+      />
+    </form>
+  )
+}
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: 'Going to salon',
+      isCompleted: false
+    },
+    {
+      text: 'Meeting with group manager',
+      isCompleted: false
+    },
+    {
+      text: 'Meeting with board of directors',
+      isCompleted: false
+    }
+  ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos); 
+  }
+
+  const deleteTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
+
+  return (
+    <div className='app'>
+      <div className='todo-list'>
+        {todos.map((todo, index) => (
+           <Todo 
+              key={index} 
+              index={index} 
+              todo={todo} 
+              completeTodo={completeTodo} 
+              deleteTodo={deleteTodo}
+            />
+        )
+        )}
+            <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  )
+}
+
+export default App
